71ae7ea896dde2d35397d95eac7e1d2810f2e00f
Judit Novak <judit.novak@canonical.com>
2023-07-16 01:36:03 +0200

    Unexpected behavior

--- src/charm.py
+++ src/charm.py
@@ -15,6 +15,7 @@ https://discourse.charmhub.io/t/4208
 import logging
 
 import ops
+from ops import ActiveStatus, SecretNotFoundError
 from ops.charm import ActionEvent
 
 # Log messages can be retrieved using juju debug-log
@@ -30,16 +31,27 @@ class SecretsTestCharm(ops.CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
 
+        self.framework.observe(self.on.start, self._on_start)
+
         self.framework.observe(self.on.set_secret_action, self._on_set_secret_action)
         self.framework.observe(self.on.get_secrets_action, self._on_get_secrets_action)
+        self.framework.observe(self.on.delete_secrets_action, self._on_delete_secrets_action)
+
+    def _on_start(self, event) -> None:
+        self.unit.status = ActiveStatus()
 
     def _on_set_secret_action(self, event: ActionEvent):
         key, value = event.params.get("key"), event.params.get("value")
-        self.set_secret(key, value)
+        event.set_results({"secret-id": self.set_secret(key, value)})
 
     def _on_get_secrets_action(self, event: ActionEvent):
         """Return the secrets stored in juju secrets backend."""
         event.set_results({"secrets": self.get_secrets()})
+
+    def _on_delete_secrets_action(self, event: ActionEvent):
+        keys = event.params.get("keys")
+        for key in keys:
+            self.delete_secret(key)
 
     @property
     def peers(self) -> ops.model.Relation:
@@ -57,31 +69,59 @@ class SecretsTestCharm(ops.CharmBase):
     def get_secrets(self) -> dict[str, str]:
         """Get the secrets stored in juju secrets backend."""
         secret_id = self.app_peer_data.get("secret-id")
-            
+
         if not secret_id:
             return {}
 
-        secret = self.model.get_secret(id=secret_id)
+        try:
+            secret = self.model.get_secret(id=secret_id)
+        except SecretNotFoundError:
+            return {}
         content = secret.get_content()
         logger.info(f"Retrieved secret {secret_id} with content {content}")
         return content
 
     def set_secret(self, key: str, value: str) -> None:
         """Set the secret in the juju secret storage."""
-        secret_id = self.app_peer_data.get("secret-id")
+        if not value:
+            return self.delete_secret(key)
 
-        content = {
-            key: value,
-        }
+        secret_id = self.app_peer_data.get("secret-id")
 
         if secret_id:
             secret = self.model.get_secret(id=secret_id)
+            content = secret.get_content()
+            content.update({key: value})
             logger.info(f"Setting secret {secret.id} to {content}")
             secret.set_content(content)
         else:
+            content = {
+                key: value,
+            }
             secret = self.app.add_secret(content)
             self.app_peer_data["secret-id"] = secret.id
             logger.info(f"Added secret {secret.id} to {content}")
+
+        return secret.id
+
+    def delete_secret(self, key: str) -> None:
+        """Remove a secret."""
+        secret_id = self.app_peer_data.get("secret-id")
+
+        if not secret_id:
+            logging.error("Can't delete any secrets as we have none defined")
+
+        secret = self.model.get_secret(id=secret_id)
+        content = secret.get_content()
+        if key in content:
+            del content[key]
+        logger.info(f"Removing {key} from secret {secret.id}")
+        logger.info(f"Remaining content is {list(content.keys())}")
+        if content:
+            secret.set_content(content)
+        else:
+            secret.remove_all_revisions()
+            del self.app_peer_data["secret-id"]
 
 
 if __name__ == "__main__":  # pragma: nocover
